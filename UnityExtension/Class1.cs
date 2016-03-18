9bddb7f5aa80ecb8d5060c2724ac22ac738908ab
tylearymf <tylearymf@163.com>
2016-03-18 17:35:13 +0800

    tylearymf

---
+++ UnityExtension/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+static public class UnityExtension
+{
+    static public void ResetTransformation(this Transform trans)
+    {
+        trans.position = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+        trans.localScale = new Vector3(1, 1, 1);
+    }
+
+    static public void ChangeGameObjectName(this GameObject go,string name)
+    {
+        go.name = name;
+    }
+}
+
